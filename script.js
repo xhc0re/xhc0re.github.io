967c577bf1d6022f058bbff0971853fe185d774e
xhc0re <hc0re@tuta.io>
2025-01-16 15:43:34 +0100

    added dark theme and button to switch themes

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+function applySystemTheme() {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (prefersDarkScheme) {
+        document.body.classList.add("dark-theme");
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", applySystemTheme);
+
+applySystemTheme();
+
 document.getElementById("kosnujButton").addEventListener("click", function () {
     const inputText = document.getElementById("inputText").value;
     const kosnujText = inputText.replace(/(po|ko)/gi, (match) => match.toUpperCase());
     document.getElementById("inputText").value = kosnujText;
+});
+
+document.getElementById("toggleThemeButton").addEventListener("click", function () {
+    document.body.classList.toggle("dark-theme");
 });
